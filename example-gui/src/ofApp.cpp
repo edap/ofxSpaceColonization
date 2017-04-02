d3f4b0a9bd17965132b501b56444e8b66f6944d2
edapx <lastexxit@gmail.com>
2017-04-02 17:19:55 +0200

    re-add example

---
+++ example-gui/src/ofApp.cpp
@@ -0,0 +1,89 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    gui.setup();
+    gui.add(selectedMaxDist.set("MaxDist", 150, 90, 300));
+    gui.add(selectedMinDist.set("MinDist", 10, 3, 90));
+    gui.add(selectedMaxDist.set("selectedLength", 5, 2, 30));
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    tree.grow();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    maybeDrawGui();
+    //camera.begin();
+    tree.draw();
+    //camera.end();
+}
+
+void ofApp::maybeDrawGui(){
+    if (drawGui) {
+        //fps
+        string msg = "\n\nfps: " + ofToString(ofGetFrameRate(), 2);
+        ofDrawBitmapStringHighlight(msg, 10, 10);
+        //gui
+        gui.draw();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if(key == 'g'){
+        drawGui = !drawGui;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
