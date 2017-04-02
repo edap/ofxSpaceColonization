189bdb7fd7f9a6ac7a66b90a1f9f54022ce9767c
edapx <lastexxit@gmail.com>
2017-04-02 16:27:41 +0200

    add setters

--- src/ofxSpaceColonization.cpp
+++ src/ofxSpaceColonization.cpp
@@ -38,61 +38,76 @@ ofxSpaceColonization::ofxSpaceColonization(){
 void ofxSpaceColonization::grow(){
     //arrivati verso la fine questo numero dovrebbe smettere di crescere
     // invece continua a salire
-    cout << branches.size() << endl;
-    for(int it=0;it<leaves.size();it++){
-        float record = 10000.0;
+    if(!done_growing){
+        cout << branches.size() << endl;
+        for(int it=0;it<leaves.size();it++){
+            float record = 10000.0;
 
-        auto closestBranchIndex = -1;
-        for(int i=0;i<branches.size();i++){
-            auto distance = leaves[it].getPosition().distance(branches[i]->getPosition());
-            if(distance < min_dist){
-                leaves[it].reached = true;
-                closestBranchIndex = -1;
-                break;
-            }else if (distance > max_dist){
-                //break;
-            }else if ((closestBranchIndex < 0) || (distance < record)){
-                closestBranchIndex = i;
-                record = distance;
+            auto closestBranchIndex = -1;
+            for(int i=0;i<branches.size();i++){
+                auto distance = leaves[it].getPosition().distance(branches[i]->getPosition());
+                if(distance < min_dist){
+                    leaves[it].reached = true;
+                    closestBranchIndex = -1;
+                    break;
+                }else if (distance > max_dist){
+                    //break;
+                }else if ((closestBranchIndex < 0) || (distance < record)){
+                    closestBranchIndex = i;
+                    record = distance;
+                }
+            }
+
+            if(closestBranchIndex>=0 && !leaves[it].reached){
+                auto dir = (leaves[it].getPosition() - branches[closestBranchIndex]->getPosition()).normalize();
+                branches[closestBranchIndex]->direction = branches[closestBranchIndex]->direction + dir;
+                branches[closestBranchIndex]->count = branches[closestBranchIndex]->count + 1;
+
+                shared_ptr<ofxSpaceColonizationBranch> nextBranch(new ofxSpaceColonizationBranch(dir));
+                if(!branches.empty()){
+                    nextBranch->setParent(branches.back());
+                    nextBranch->move(branches[closestBranchIndex]->direction * branch_length);
+                    addBranchToMesh(nextBranch);
+                }
+            }
+
+            if(leaves[it].reached){
+                // in futuro, puoi tenere queste foglie, e disegnare solo quelle
+                // che vengono reached
+                leaves.erase(leaves.begin()+it);
             }
         }
 
-        if(closestBranchIndex>=0 && !leaves[it].reached){
-            auto dir = (leaves[it].getPosition() - branches[closestBranchIndex]->getPosition()).normalize();
-            branches[closestBranchIndex]->direction = branches[closestBranchIndex]->direction + dir;
-            branches[closestBranchIndex]->count = branches[closestBranchIndex]->count + 1;
-
-            shared_ptr<ofxSpaceColonizationBranch> nextBranch(new ofxSpaceColonizationBranch(dir));
-            if(!branches.empty()){
-                nextBranch->setParent(branches.back());
-                nextBranch->move(branches[closestBranchIndex]->direction * branch_length);
+        for (int i = 0; i<branches.size(); i++) {
+            if(branches[i]!= nullptr && branches[i]->count > 0){
+                // controllare qui, shiffman dice di aggiungere 1 al count (min 44.52)
+                auto newDir = branches[i]->direction / (float(branches[i]->count));
+                //auto newDir = branches[i]->direction / (float(branches[i]->count + 1));
+                shared_ptr<ofxSpaceColonizationBranch> nextBranch(new ofxSpaceColonizationBranch(newDir));
+                nextBranch->setParent(branches[i]);
+                nextBranch->move(newDir * branch_length);
                 addBranchToMesh(nextBranch);
+                //add nextBranch to mesh
+                branches.push_back(nextBranch);
             }
-        }
-
-        if(leaves[it].reached){
-            // in futuro, puoi tenere queste foglie, e disegnare solo quelle
-            // che vengono reached
-            leaves.erase(leaves.begin()+it);
-        }
-    }
-
-    for (int i = 0; i<branches.size(); i++) {
-        if(branches[i]!= nullptr && branches[i]->count > 0){
-            auto newDir = branches[i]->direction / float(branches[i]->count);
-            shared_ptr<ofxSpaceColonizationBranch> nextBranch(new ofxSpaceColonizationBranch(newDir));
-            nextBranch->setParent(branches[i]);
-            nextBranch->move(newDir * branch_length);
-            addBranchToMesh(nextBranch);
-            //add nextBranch to mesh
-            branches.push_back(nextBranch);
-        }
-        if(branches[i]!= nullptr){
-            branches[i]->reset();
+            if(branches[i]!= nullptr){
+                branches[i]->reset();
+            }
         }
     }
 
 }
+void ofxSpaceColonization::setMinDist(int _min_dist){
+    min_dist = _min_dist;
+};
+
+void ofxSpaceColonization::setMaxDist(int _max_dist){
+    max_dist = _max_dist;
+};
+
+void ofxSpaceColonization::setBranchLength(int _length){
+    branch_length = _length;
+};
 
 void ofxSpaceColonization::draw(){
     for(auto l:leaves){
--- src/ofxSpaceColonization.h
+++ src/ofxSpaceColonization.h
@@ -9,13 +9,17 @@ public:
 	ofxSpaceColonization();
     void grow();
     void draw();
+    void setMinDist(int min_dist);
+    void setMaxDist(int max_dist);
+    void setBranchLength(int branch_length);
 private:
     void addBranchToMesh(shared_ptr<ofxSpaceColonizationBranch> branch);
     vector<ofxSpaceColonizationLeaf> leaves;
     std::vector<shared_ptr<ofxSpaceColonizationBranch>> branches;
     int n_leaves = 100;
     int max_dist = 150;
-    int min_dist = 13;
+    int min_dist = 10;
     int branch_length = 5;
+    bool done_growing = false;
     ofMesh mesh;
 };
